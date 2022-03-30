5c55ccecd709d758a6e8619b922fb7a52381e19d
maria <mabdallaoui6478@students.mc3.edu>
2022-03-30 10:19:52 -0400

    Add Todo functions with their tests

--- src/App.js
+++ src/App.js
@@ -20,3 +20,69 @@ export function highestScore(students) {
   
     return result;
   }
+
+  export function factorial(n) {
+    if ((n === 0) || (n === 1))
+      return 1;
+    else
+      return (n * factorial(n - 1));
+  }
+
+  // *******************************************************************Week 7 assignment*********************************************************
+
+ const todos = [{
+    id: 1,
+    text: "React Hooks in Depth",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: "Write Articles @ Medium",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "Share article at Reddit",
+    isCompleted: false,
+  }];
+
+// This function returns the todos list
+
+export function todosList(){
+
+  return todos;
+  }
+
+
+// This function add a todo to a todos list
+
+export function addTodo (id, text) {
+  let newTodoItems = [...todos];
+  let todo = {id: id,text:text,isCompleted:false};        
+  newTodoItems.push(todo);
+  return newTodoItems;
+} 
+
+// This function remove a todo to a todos list
+
+export function removeTodo(id){
+
+  const newTodoItems = [...todos];
+  const index = newTodoItems.findIndex((todo) => todo.id === id);
+  newTodoItems.splice(index, 1); 
+  return newTodoItems;
+}
+
+// This function makes all the "isCompleted" property of all objects true.
+
+export function updateTodos(){
+  
+  todos.forEach(function(e){
+    e.isCompleted=true;
+    })
+     return todos;
+
+}
+
+
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { add, total, highestScore } from './App';
+import { add, total, highestScore, factorial, todosList, addTodo, removeTodo, updateTodos } from './App';
 
 // https://jestjs.io/docs/expect#expectvalue
 // The expect function is used every time you want to test a value.
@@ -47,6 +47,14 @@ test('highest score', () => {
   expect(result).toEqual({ name: 'Shane Carey', score: 9.5 });
 });
 
+// test factorial function
+
+test('factorial number: verify correct return values',()=>{
+    
+     expect(factorial(1)).toBe(1);
+     expect(factorial(3)).toBe(6);
+});
+
 
 // Integration test - testing a function that relies on another function (multiple things)
 //  - testing add() then total() function
@@ -56,3 +64,96 @@ test('total', () => {
   expect(total(2, 10)).toBe('$12');
   expect(total(8, 50)).toBe('$58');
 });
+
+// *********************************************************** Week7 assignment ****************************************************************
+
+
+// test todosList function
+test('todosList - if function defined?', () => {
+  expect(todosList).toBeDefined(); 
+});
+
+// test todosList function
+test('todosList - is function returning a value?', () => {
+  expect(todosList()).toBeDefined();
+});
+
+// test todosList function
+test('todosList - verify correct return values', () => {
+  
+  const FinalResult = [{id: 1, text: "React Hooks in Depth", isCompleted: false},
+                       {id: 2, text: "Write Articles @ Medium", isCompleted: false},
+                       {id: 3, text: "Share article at Reddit",  isCompleted: false},
+  
+                       ];
+  expect(todosList()).toEqual(FinalResult);
+      
+});
+
+  // ********************************************************************************
+
+// test addTodo function
+test('addTodo - if function defined?', () => {
+  expect(addTodo).toBeDefined(); 
+});
+
+// test addTodo function
+
+test('addTodo - is function returning a value?', () => {
+  expect(addTodo(5, "Do homework")).toBeDefined();
+});
+
+// test addTodo function
+test('addTodo - verify correct return values', () => {
+
+const result = [{id: 1, text: "React Hooks in Depth", isCompleted: false},
+                {id: 2, text: "Write Articles @ Medium", isCompleted: false},
+                {id: 3, text: "Share article at Reddit",  isCompleted: false},
+                {id: 4, text: "Learning React JS", isCompleted: false }
+               ];
+ expect(addTodo(4,"Learning React JS")).toEqual(result);
+
+});
+
+// ********************************************************************************
+
+// test removeTodo function
+test('removeTodo - if function defined?', () => {
+  expect(removeTodo).toBeDefined(); 
+}); 
+
+// test removeTodo function
+test('removeTodo - is function returning a value?', () => {
+  expect(removeTodo(3)).toBeDefined();
+});
+
+// test removeTodo function
+test('removeTodo - verify correct return values', () => {
+  const result = [
+    {id: 1, text: "React Hooks in Depth", isCompleted: false},
+    {id: 2, text: "Write Articles @ Medium", isCompleted: false}    
+    ]
+     expect(removeTodo(3)).toEqual(result);
+    
+  });
+
+// ********************************************************************************
+
+  // test updateTodos function
+test('updateTodos - if function defined?', () => {
+  expect(updateTodos).toBeDefined(); 
+});
+
+// test updateTodos function
+test('updateTodos - is function returning a value?', () => {
+  expect(updateTodos()).toBeDefined();
+});
+
+// test updateTodos function
+test('updateTodos - verify correct return values', () => {
+  const expectedResult = [{id: 1, text: "React Hooks in Depth", isCompleted: true},
+                          {id: 2, text: "Write Articles @ Medium", isCompleted: true},
+                          {id: 3,text: "Share article at Reddit",  isCompleted: true}
+                         ];
+     expect(updateTodos()).toEqual(expectedResult);
+      });
